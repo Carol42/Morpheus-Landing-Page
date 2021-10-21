df86b284dbbaca916b55ff81bc0a5265c9e3f8fa
Caroline Oliveira <63017741+Carol42@users.noreply.github.com>
2021-10-21 15:46:09 -0300

    primeira versão do site

---
+++ funcoes.js
@@ -0,0 +1,31 @@
+// top button
+
+button = document.getElementById("topButton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+} 
+
+// escreve título
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
